5de523c608ac9d198ac225c8e2a77e26085160ed
Taras <taras.chr@gmail.com>
2019-11-14 09:36:28 +0200

    Add full mapper and interface

---
+++ src/EntityMapper.php
@@ -0,0 +1,173 @@
+<?php
+declare(strict_types=1);
+
+namespace EntityManager;
+
+use Doctrine\Annotations\AnnotationReader;
+use Doctrine\Common\Collections\ArrayCollection;
+use function is_callable;
+use function ucfirst;
+
+class EntityMapper
+{
+    /**
+     * @var ArrayCollection
+     */
+    private $collection;
+
+    /**
+     * @var Entity
+     */
+    private $entity;
+
+    /**
+     * @var array<string,Mapper>
+     * @var Mapper[]
+     */
+    private $subMapper = [];
+
+    /**
+     * @var array
+     */
+    private $mapped = [];
+
+    /**
+     * Mapper constructor.
+     * @param ArrayCollection $collection
+     * @param Entity $entity
+     */
+    public function __construct(ArrayCollection $collection, Entity $entity)
+    {
+        $this->collection = $collection;
+        $this->entity = $entity;
+    }
+
+    /**
+     * @param ArrayCollection $collection
+     * @param Entity $entity
+     * @return EntityMapper
+     */
+    public static function createFrom(ArrayCollection $collection, Entity $entity): EntityMapper
+    {
+        return new self($collection, $entity);
+    }
+
+    /**
+     * @param ArrayCollection $collection
+     * @param Entity $entity
+     * @throws \Doctrine\Annotations\AnnotationException
+     * @throws \ReflectionException
+     */
+    private function map(ArrayCollection $collection, Entity $entity)
+    {
+        $entityReflection = new \ReflectionClass($entity);
+        $entityProperties = $entityReflection->getProperties(\ReflectionProperty::IS_PRIVATE);
+        $collectionProperties = $collection->getKeys();
+        $annotationReader = new AnnotationReader();
+
+        foreach ($entityProperties as $property) {
+            $fieldName = $this->getFieldName($property, $annotationReader);
+            $propertyName = $property->getName();
+
+            if (in_array($fieldName, $collectionProperties, true)) {
+                $methodName = 'set' . ucfirst($propertyName);
+                $value = $collection->get($fieldName);
+
+                if (isset($this->subMapper[$propertyName]) && $this->subMapper[$propertyName] instanceof Mapper) {
+                    $value = $this->bindSubMapper($this->subMapper[$propertyName], $value, $propertyName);
+                }
+
+                if (is_callable([$entity, $methodName])) {
+                    $entity->$methodName($value);
+                }
+            }
+        }
+
+        $this->mapped[] = $entity;
+    }
+
+    /**
+     * @param Mapper $mapper
+     * @param $value
+     * @param string $property
+     * @return mixed
+     */
+    private function bindSubMapper(Mapper $mapper, $value, string $property)
+    {
+        if ($value instanceof ArrayCollection) {
+            $mapper->setCollection($value);
+        } elseif (is_array($value)) {
+            $mapper->setCollection(new ArrayCollection($value));
+        } else {
+            $mapper->setCollection(new ArrayCollection([$property => $value]));
+        }
+
+        return $mapper->getMapped();
+    }
+
+    /**
+     * @return Entity[]
+     * @throws \Doctrine\Annotations\AnnotationException
+     * @throws \ReflectionException
+     */
+    public function mapList(): array
+    {
+        foreach ($this->collection as $item) {
+            if ($item instanceof ArrayCollection) {
+                $this->map($item, $this->entity);
+            } elseif (is_array($item)) {
+                $this->map(new ArrayCollection($item), $this->entity);
+            }
+        }
+
+        return $this->mapped;
+    }
+
+    /**
+     * @throws \Doctrine\Annotations\AnnotationException
+     * @throws \ReflectionException
+     */
+    public function mapSingle()
+    {
+        if ($this->collection instanceof ArrayCollection) {
+            $this->map($this->collection, $this->entity);
+        } elseif (is_array($this->collection)) {
+            $this->map(new ArrayCollection($this->collection), $this->entity);
+        }
+
+        return array_shift($this->mapped);
+    }
+
+    /**
+     * @param string $property
+     * @param Mapper $mapper
+     * @return EntityMapper
+     */
+    public function setSubMapper(string $property, Mapper $mapper): EntityMapper
+    {
+        $this->subMapper[$property] = $mapper;
+        return $this;
+    }
+
+    /**
+     * @param \ReflectionProperty $property
+     * @param AnnotationReader $annotationReader
+     * @return string
+     */
+    private function getFieldName(\ReflectionProperty $property, AnnotationReader $annotationReader): string
+    {
+        /** @var Property|null $annotation */
+        $annotation = $annotationReader->getPropertyAnnotation(
+            $property,
+            Property::class
+        );
+
+        if ($annotation) {
+            $fieldName = $annotation->field;
+        } else {
+            $fieldName = $property->getName();
+        }
+
+        return $fieldName;
+    }
+}
